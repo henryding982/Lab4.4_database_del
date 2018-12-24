08ea5f720569e8d7e5f907a21a5202f1053eedff
Ding <henryding982@gmail.com>
2018-12-24 13:19:56 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/demoroom/User.java
@@ -0,0 +1,49 @@
+package my.edu.tarc.demoroom;
+
+
+//TODO 3: Create an Entity class to present a record
+
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "user")
+public class User {
+    @PrimaryKey
+    @NonNull
+    private String phone;
+
+    @ColumnInfo(name = "first_name")
+    private String firstName;
+
+    @ColumnInfo(name =  "last_name")
+    private String lastName;
+
+    @NonNull
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(@NonNull String phone) {
+        this.phone = phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
+
---
+++ app/src/main/java/my/edu/tarc/demoroom/update.java
@@ -0,0 +1,18 @@
+package my.edu.tarc.demoroom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class update extends AppCompatActivity {
+
+    EditText phoneText;
+    EditText nameText;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+
+    }
+}
